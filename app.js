19713458c4555f6dc720560b7aeddaa1a2d3a50b
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-03-18 20:05:10 +0530

    Finally we are ready to commit our changes to the repository.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get("/", (req, res)=>{
 
-  const url= "https://api.openweathermap.org/data/2.5/weather?q=Bhubaneshwar&appid=e02502865e18d2e98d6fb89b70a6de7c&units=metricapi.openweathermap.org/data/2.5/weather?q=Ranchi&appid=e02502865e18d2e98d6fb89b70a6de7c&units=metric"
+  const url= "https://api.openweathermap.org/data/2.5/weather?q=Brahmapur&appid=e02502865e18d2e98d6fb89b70a6de7c&units=metricapi.openweathermap.org/data/2.5/weather?q=Ranchi&appid=e02502865e18d2e98d6fb89b70a6de7c&units=metric"
   
   
   https.get(url , (res2)=>{ 
@@ -17,20 +17,22 @@ app.get("/", (req, res)=>{
   
   res2.on("data",(data)=>{
     console.log(data);
+    
     const weatherData = JSON.parse(data);
     console.log(weatherData);
-
+    const imgURL="https://openweathermap.org/img/w/"+weatherData.weather[0].icon+".png";
     console.log("The temperature in "+weatherData.name+" is "+weatherData.main.temp);
     console.log("The weather is "+weatherData.weather[0].description);
     console.log("The humidity is "+weatherData.main.humidity);
     console.log("The wind speed is "+weatherData.wind.speed);
     console.log("The pressure is "+weatherData.main.pressure);
     res.write("<table><th>The Weather Statistics for "+weatherData.name+" are as follows :- </th><tr><td>The weather is <b>"+weatherData.weather[0].description+"</b></td><td>The humidity is <b>"+weatherData.main.humidity+"</b></td></tr>  <tr><td>The wind speed is <b>"+weatherData.wind.speed+"</b></td><td>The pressure is <b>"+weatherData.main.pressure+"</b></td></tr></table>");
+    res.write("<img src="+imgURL+">");
     res.send();
     //res.send("The temperature in "+weatherData.name+" is "+weatherData.main.temp+" The weather is "+weatherData.weather[0].description+" The humidity is "+weatherData.main.humidity+" The wind speed is "+weatherData.wind.speed+" The pressure is "+weatherData.main.pressure+" The pressure is "+weatherData.main.pressure+");
   });
 
-  });
+ });
 
  
 });
